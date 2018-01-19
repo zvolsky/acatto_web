788065ba0e5e077d9daef7f9203ddd94a2cf342f
Mirek Zvolský <zvolsky@seznam.cz>
2018-01-19 15:00:05 +0100

    vic na strance produktu

--- acatto/views.py
+++ acatto/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render_to_response
 
 from django.utils.translation import gettext_lazy as _
 
-from .models import Product, Offer
+from .models import Product, ProductLoc, Offer
 
 
 def products(request):
@@ -16,5 +16,6 @@ def products(request):
 
 
 def product(request, product_id):
-    product = Product.objects.get(pk=int(product_id))
+    # product = Product.objects.get(pk=int(product_id))
+    product = ProductLoc.objects.filter(product=int(product_id)).filter(lang__startswith='cs_CZ')[0]
     return render_to_response('product.html', {'product': product})
